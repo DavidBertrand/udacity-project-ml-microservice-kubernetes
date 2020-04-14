319e3f18904713f0064be1213cf43fa26b822c4b
david <bertrand282@gmail.com>
2020-04-14 00:13:56 -0400

    feat: Task 6: Deploy with Kubernetes and Save Output Logs

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 dockerpath="bertrand282/ml-microservice"
+
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
+
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="bertrand282/ml-microservice"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+podname="ml-microservice-pod"
+kubectl run $podname --image=$dockerpath --port=80 --generator=run-pod/v1
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $podname 8000:80
